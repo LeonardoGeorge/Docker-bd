c291271a17b374542a5cebbb6dae0fb5dc151cad
Leonardo George <leonardogeorge30@gmail.com>
2025-09-08 12:04:04 -0300

    node22-dev-run

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
-    res.end('Hello, Docker... :) API-Node; Criado com sucesso! \n');
+    res.end('Hello, Docker... :) API-Node; Criado com sucesso, agora com volumes! \n');
 });
 
 server.listen(3000);
